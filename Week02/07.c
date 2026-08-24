6f36e59d731eaab5d7798800e50dd02a5376cbaa
kwon minwoo <githubm2nwoo1234@gmail.com>
2026-08-24 19:49:07 +0900

    07.c
    Add a new C program demonstrating number formats and floating-point output.

---
+++ Week02/07.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d %d\n", 010, 015); //8진수 
+	printf("%d %d\n", 10, 15); //10진수 
+	printf("%d %d\n", 0X1a, 0x15); //16진수 
+	
+	printf("%f, ", 2.71828);
+	printf("%f, ", 2.171828E+2);
+	printf("%f\n", 2.71828e-2);
+	return 0;
+}
