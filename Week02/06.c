38e35c30ff9202bb30ca296a915bef6bce6abfe7
kwon minwoo <githubm2nwoo1234@gmail.com>
2026-08-24 18:56:55 +0900

    06.c

---
+++ Week02/06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int ch = '#';
+	printf("%d\n", ch); //십진 코드 값 
+	printf("%c\n", ch); //문자 출력 
+	
+	long long pop1 = 51800000; //대한민국 인구 5180만 
+	long long pop2 = 7716600000; //전세계 인구 약 77억 
+	printf("%lld %lld\n", pop1, pop2);
+	
+	return 0;
+}
