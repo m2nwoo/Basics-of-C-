cf173dd840f6f9c142d253a173de98710f21fb76
kwon minwoo <githubm2nwoo1234@gmail.com>
2026-08-24 18:54:49 +0900

    01.c

---
+++ Week02/01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int year = 2026; //선언 동시에 초기화 
+	
+	int credits;
+	
+	credits = 15; //선언 변수에 초기화 
+	
+	printf("%d년도\n", year);
+	printf("이수학점: %d학점\n", credits);
+	
+	return 0;
+}
