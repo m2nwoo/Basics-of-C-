071896f3fe1b0b8058e9f6f6f32cf76059ffc8de
Isaac del Toro Romero <githubm2nwoo1234@gmail.com>
2026-09-02 13:22:39 +0900

    08.c
    Add a C program to convert octal input to decimal and hexadecimal.

---
+++ Week03/08.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int oct;
+	printf("8진수 정수 입력 >>");
+	scanf("%o", oct);
+	
+	printf("%o %d %x\n", oct, oct, oct);
+	printf("%o %i %#X\n", oct, oct, oct);
+	 
+	return 0;
+}
